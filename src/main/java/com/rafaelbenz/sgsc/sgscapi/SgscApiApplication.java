9b6d837bf3224eaf9fc07ddbe3fd6c66d2baab0a
Rafael Benzaquem Neto <rafaelbenzaquem@ulbra.edu.br>
2019-10-10 17:04:34 -0300

    comit inicial do projeto

---
+++ src/main/java/com/rafaelbenz/sgsc/sgscapi/SgscApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelbenz.sgsc.sgscapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgscApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgscApiApplication.class, args);
+    }
+
+}
